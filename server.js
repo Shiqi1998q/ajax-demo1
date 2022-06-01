459a5cb7594697a7471f77aef5f0601a25f7c54c
shiqi <813385610@qq.com>
2022-06-01 20:12:01 +0800

    1

--- server.js
+++ server.js
@@ -27,12 +27,10 @@ var server = http.createServer(function (request, response) {
         response.write(`
           
           <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>ajax</title>
     
 </head>
